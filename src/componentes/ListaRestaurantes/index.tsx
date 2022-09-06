79174107ee90b757c599c2996e1c729627dde1c0
joabesimao <simaojoabecosta@gmail.com>
2022-09-06 14:55:29 -0300

    adiciona busca  restaurante

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios, { AxiosRequestConfig } from "axios";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
@@ -9,6 +9,38 @@ import { IPaginacao } from "../../interfaces/IPaginacao";
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState("");
+  const [busca, setBusca] = useState("");
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+    axios
+      .get<IPaginacao<IRestaurante>>(url, opcoes)
+      .then((resposta) => {
+        setRestaurantes(resposta.data.results);
+        setProximaPagina(resposta.data.next);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  };
+
+  interface IParametrosBusca {
+    ordering?: string;
+    search?: string;
+  }
+
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    const opcoes = {
+      params: {} as IParametrosBusca,
+    };
+    if (busca) {
+      opcoes.params.search = busca;
+    }
+    carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
+  };
+  useEffect(() => {
+    carregarDados("http://localhost:8000/api/v1/restaurantes/");
+  }, []);
 
   useEffect(() => {
     //obter restautante
@@ -42,9 +74,18 @@ const ListaRestaurantes = () => {
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
+      <form onSubmit={buscar}>
+        <input
+          type="text"
+          value={busca}
+          onChange={(evento) => setBusca(evento.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
+
       {proximaPagina && <button onClick={verMais}> Ver Mais </button>}
     </section>
   );
